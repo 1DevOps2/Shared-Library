4ba07f303e59c0c178aab21765cb4f045df8610d
1DevOps2 <anaskhandevops@gmail.com>
2022-08-14 18:15:31 +0000

    changing lines

--- vars/MyInfo.groovy
+++ vars/MyInfo.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
+/* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
 def call() {
-echo "Name: Anas Khan"
-echo "Address: Multan, Punjab, Pakistan"
-echo "Profession: IT"
-echo "Age: 22"
- 
+    echo 'Name: Anas Khan'
+    echo 'Address: Multan, Punjab, Pakistan'
+    echo 'Profession: IT'
+    echo 'Age: 22'
 }
--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,5 @@
 #!/usr/bin/env groovy
+/* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
 def call(String name = 'human') {
-echo "Hello, ${name}."
+    echo "Hello, ${name}."
 }
